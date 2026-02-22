b331490e28ac43849e48ca467023df85e4866bdb
dan94k <dan94br@gmail.com>
2026-02-21 23:36:51 -0300

    Utilizando Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: process.env.VITE_BASE_URL ?? "/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
